886eaa355b1d8e2c25527e1cb6b1af5db3f7a42d
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-07-21 15:34:05 +0200

    Aggiungere i file di progetto.

---
+++ Requesting Data/Program.cs
@@ -0,0 +1,18 @@
+﻿using Requesting_Data;
+
+RetriveUrl retriveUrl = new RetriveUrl();  
+
+Console.Write("inserisci un url: ");
+string inputURL = Console.ReadLine();
+
+Console.WriteLine("\nsto stampando il contenuto dell'URL:\n");
+Console.WriteLine(await retriveUrl.GetDescription(inputURL));
+
+
+
+
+
+
+
+
+
---
+++ Requesting Data/RetriveUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requesting_Data
+{
+    public class RetriveUrl
+    {
+        public async Task<string> GetDescription(string url)
+        {
+            var httpClient= new HttpClient();
+            var uri= new Uri(url);
+            var response= await httpClient.GetAsync(uri);
+            
+            foreach(var header in  response.Headers)
+            {
+                Console.WriteLine(header.Key + "= " + string.Join(", ", header.Value));
+            }
+
+            var result= await response.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
+}
